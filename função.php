8cebb83ce3d275f4c6c6a5f96ab9a5b9cf74dd7f
negri007 <negrithc@gmail.com>
2025-09-02 10:31:01 -0300

    função

---
+++ função.php
@@ -0,0 +1,20 @@
+<?php
+
+// Função para somar dois números
+function adicionar($a, $b) {
+    return $a + $b;
+}
+
+// Função para subtrair dois números
+function subtrair($a, $b) {
+    return $a - $b;
+}
+
+// Função para calcular a raiz quadrada de um número
+function raizQuadrada($a) {
+    if ($a < 0) {
+        return "Erro: não é possível calcular a raiz quadrada de número negativo.";
+    }
+    ret
+}
+?>
